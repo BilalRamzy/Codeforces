787c48d98d98e95d27e0cbbb1da59c81734914ea
blalramzy1 <blalramzy2019@gmail.com>
2025-07-22 15:08:54 +0300

    binary search

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,43 +1,61 @@
-#include<iostream>
-#include<vector>
-<<<<<<< HEAD
-#include<algorithms>
-=======
-#include<algorithm>
->>>>>>> 1b6bcbe (first commit)
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
 using namespace std;
 
-
-
-
-int main()
-{
-    int n, q; cin >> n >> q;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     
-
-    vector<int> a(n);
-    for(int i = 0; i < n; i++){
-        cin >> a[i];
+    int n, q;
+    cin >> n >> q;
+    
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
-
-    sort(a.begin(), a.end());
-
-    while(q--)
-    {
-        int x; cin >> x;
-
-<<<<<<< HEAD
-        if(binary_search(a.begin(), a.end(), x)
-=======
-        if(binary_search(a.begin(), a.end(), x))
->>>>>>> 1b6bcbe (first commit)
-        {
-            cout << "found" << endl;        
-        } else {
-        cout << "not found" << endl;
+    
+    sort(arr.begin(), arr.end());
+    
+    // Process each query
+    for (int i = 0; i < q; i++) {
+        string operation;
+        int x;
+        cin >> operation >> x;
+        
+        if (operation == "binary_search") {
+          
+            bool found = binary_search(arr.begin(), arr.end(), x);
+            if (found) {
+                cout << "found" << endl;
+            } else {
+                cout << "not found" << endl;
+            }
         }
-
+        else if (operation == "lower_bound") {
+    
+            auto it = lower_bound(arr.begin(), arr.end(), x);
+            
+            if (it == arr.end()) {
+                // No element >= x found
+                cout << -1 << endl;
+            } else {
+                cout << *it << endl;
+            }
+        }
+        else if (operation == "upper_bound") {
+            auto it = upper_bound(arr.begin(), arr.end(), x);
+            
+            if (it == arr.end()) {
+                // No element > x found
+                cout << -1 << endl;
+            } else {
+                cout << *it << endl;
+            }
+        }
     }
+    
     return 0;
-
 }
+
